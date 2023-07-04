1d34583d4f646dea5d9d3c5c57a9027b7e1b55e1
aesperer <piyrw9754@gmail.com>
2023-07-04 13:54:32 +0900

    consumer broker

--- src/main/kotlin/esperer/kafkademo/controller/KafkaController.kt
+++ src/main/kotlin/esperer/kafkademo/controller/KafkaController.kt
@@ -1,5 +1,7 @@
 package esperer.kafkademo.controller
 
+import esperer.kafkademo.service.KafkaService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,7 +11,8 @@ class KafkaController(
     private val kafkaService: KafkaService
 ) {
 
+    @GetMapping("/insert")
     fun insert(): String {
-
+        return kafkaService.insert()
     }
 }
